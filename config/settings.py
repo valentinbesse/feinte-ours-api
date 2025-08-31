34c2a52711dcc7b93b9c21ded6f4d2c69ab8a24b
Valentin Besse <valentinbesse@aumbox.net>
2025-08-31 02:43:07 +0200

    Utilisation de la base de données mongodb

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'feinte-ours',
     }
 }
 
